2a7f6d3e968e6946d89dfc3d376531a8a9951a54
kele1221 <47175706@qq.com>
2026-09-04 16:29:17 +0800

    chore(fork): bump fork version to 0.5.65-k.14

--- src/shared/constants/fork.js
+++ src/shared/constants/fork.js
@@ -7,6 +7,6 @@ export const FORK_CONFIG = {
   upstreamOwner: "decolua",
   upstreamRepo: "9router",
   upstreamBranch: "master",
-  version: "0.5.59-k.13",
+  version: "0.5.65-k.14",
   repositoryUrl: "https://github.com/kele1221/9router",
 };
